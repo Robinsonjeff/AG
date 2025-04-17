b1b5e9f40d3b26661e6cb800e11ad65ca57eb54b
Cole Robinson <33127273+Robinsonjeff@users.noreply.github.com>
2025-04-17 11:36:43 -0500

    Update App.js
    added useEffect import

--- alphaguard/src/App.js
+++ alphaguard/src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { colors } from "../src/Components/Colors";
 import navImage from "./Assets/header.PNG";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
